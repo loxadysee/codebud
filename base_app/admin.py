18b66058f73cc8faf3a0333ca7e02846c430852e
[loxadysee] <[rihalabdou@gmail.com]>
2024-10-21 14:39:22 +0100

    new models

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room
+from .models import Room, Topic, Message
 
 admin.site.register(Room) # register the model (room) to the admin site
+admin.site.register(Topic) # register the model (topic) to the admin site
+admin.site.register(Message) # register the model (message) to the admin site
--- base_app/models.py
+++ base_app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-<<<<<<< HEAD
+
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True) # save the time when the record is updated 
@@ -11,7 +18,16 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
-=======
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content[0:50]
+
 # Create your models here.test test hih test again
 
->>>>>>> 5212ec57dbe86aa42727d6ad5f794457450b0de7
